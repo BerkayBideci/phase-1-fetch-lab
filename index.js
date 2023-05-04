ef90cdc8e897ceea2a05b9ecfe54c77520d1cba4
Berkay Bideci <berkaybideci@gmail.com>
2023-05-04 16:17:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -10,8 +11,20 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  const pages = []
+  books.forEach(book => pages.push(book.numberOfPages))
+  console.log(pages.reduce((accumulator, currentValue) => accumulator + currentValue, 0))
+  console.log(books[4])
+  const characters = []
+  books.forEach(book => characters.push(book.characters))
+  for (let character of characters) {
+    for (let characterNested of character) {
+      if (characterNested === "https://anapioficeandfire.com/api/characters/1031")
+        console.log(characterNested)
+    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
